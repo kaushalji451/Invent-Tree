9322b262e818c2e15ed64cbd88f767febfd92831
Avanindra <heyavanindra@gmail.com>
2025-07-11 00:04:59 +0530

    added api route

---
+++ src/app/api/route.ts
@@ -0,0 +1,6 @@
+export function GET(request) {
+  return new Response(JSON.stringify({ message: "Hello, World!" }), {
+    status: 200,
+  });
+    
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import cn from '@/utils/cn'
+import React from 'react'
+
+const Navbar = ({className}) => {
+  return (
+    <div className={cn("",className)}>
+        
+    </div>
+  )
+}
+
+export default Navbar
