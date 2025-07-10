a06fead4b92f9404e77f9eb23901c53e9607adef
Avanindra <heyavanindra@gmail.com>
2025-07-10 16:17:59 +0530

    init

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import cn from "@/utils/cn";
+
+
+export const Navbar = ({className}) => {
+  return (
+    <div className={cn("bg-neutral-500 h-screen w-full",className)}>
+        
+    </div>
+  )
+}
+
