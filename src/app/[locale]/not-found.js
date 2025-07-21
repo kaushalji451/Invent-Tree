3b4cda34775c7e0d1a1abf2d97cd8c55471ce33e
Abhishek kaushal <abhishekkaushal2526@gmail.com>
2025-07-21 14:34:34 +0530

    done

--- src/app/[locale]/not-found.js
+++ src/app/[locale]/not-found.js
@@ -15,7 +15,7 @@ export default function NotFound() {
               <h1 className="text-8xl font-bold text-black drop-shadow-lg absolute top-20">404</h1>
             </div>
             <div className="mt-[-50px]">
-              <h3 className="text-2xl font-semibold mt-4">Look like you're lost</h3>
+              <h3 className="text-2xl font-semibold mt-4">Look like you&apos;re lost</h3>
               <p className="text-gray-600 mt-2">The page you are looking for is not available!</p>
                <Link href="/">
                 <button type="button" className="mt-4 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Go to Home</button>
